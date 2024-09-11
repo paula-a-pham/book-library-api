d09f4f659d6910d6a8129d6a3c758819870902da
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-11 12:05:16 +0300

    Code refactoring

--- BusinessLogic/ServicesConfigurations/ApplicationDbContextConfiguration.cs
+++ BusinessLogic/ServicesConfigurations/ApplicationDbContextConfiguration.cs
@@ -1,19 +1,13 @@
 ﻿using Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BusinessLogic.ServicesConfigurations
+namespace BusinessLogic.ServicesConfigurations;
+
+public static class ApplicationDbContextConfiguration
 {
-    public static class ApplicationDbContextConfiguration
+    public static void ApplicationDbContextConfigurator(this IServiceCollection services, string connectionString)
     {
-        public static void ApplicationDbContextConfigurator(this IServiceCollection services, string connectionString)
-        {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-        }
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
     }
 }
