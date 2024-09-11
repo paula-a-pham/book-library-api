647b4700c1d6e0e041a840d59c5edde2b9034ae2
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-11 12:34:17 +0300

    Code refactoring

--- Data/EntitiesConfigurations/BookEntityTypeConfiguration.cs
+++ Data/EntitiesConfigurations/BookEntityTypeConfiguration.cs
@@ -1,22 +1,16 @@
 ﻿using Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Data.EntitiesConfigurations
+namespace Data.EntitiesConfigurations;
+
+internal class BookEntityTypeConfiguration : IEntityTypeConfiguration<Book>
 {
-    internal class BookEntityTypeConfiguration : IEntityTypeConfiguration<Book>
+    public void Configure(EntityTypeBuilder<Book> builder)
     {
-        public void Configure(EntityTypeBuilder<Book> builder)
-        {
-            // Select Max Length
-            builder.Property(b => b.Title).HasMaxLength(100);
-            builder.Property(b => b.Summary).HasMaxLength(2000);
-            builder.Property(b => b.Language).HasMaxLength(20);            
-        }
+        // Select Max Length
+        builder.Property(b => b.Title).HasMaxLength(100);
+        builder.Property(b => b.Summary).HasMaxLength(2000);
+        builder.Property(b => b.Language).HasMaxLength(20);            
     }
 }
