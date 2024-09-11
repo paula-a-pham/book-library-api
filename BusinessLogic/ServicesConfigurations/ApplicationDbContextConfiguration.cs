045607a1b051fc839ad949bc245ad47ef0dcf488
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-11 11:33:54 +0300

    Create extension method for application db context configuration in business logic layer

---
+++ BusinessLogic/ServicesConfigurations/ApplicationDbContextConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.ServicesConfigurations
+{
+    public static class ApplicationDbContextConfiguration
+    {
+        public static void ApplicationDbContextConfigurator(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
